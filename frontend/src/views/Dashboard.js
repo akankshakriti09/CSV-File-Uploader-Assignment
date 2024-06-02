07cd8fbfc0f9ca7c65b8aa37b6964b1602b2c3da
Akanksha <akankshakriti09@gmail.com>
2024-06-02 14:51:11 +0530

    css changed

--- frontend/src/views/Dashboard.js
+++ frontend/src/views/Dashboard.js
@@ -116,7 +116,7 @@ function Dashboard() {
           <div className="row">
             <main
               role="main"
-              className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4"
+              className="col pt-3 px-4"
             >
               <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
                 <h1 className="h2">My Dashboard</h1>
